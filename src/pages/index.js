b956ba53b0fb443aba3d702170f2f96bcfa0ae89
TheLordHighExecutioner <happyjoe2008@gmail.com>
2018-02-04 03:18:34 -0600

    first textgit add --all

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Chris Wig for President</h1>
+    <p>Welcome to your new  site.</p>
+    <p>Thanks for the internship help.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
